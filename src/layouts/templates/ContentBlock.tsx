fcbba3c283436869765d5fa3c53de3784ef7a44b
Mehtab "Shahan" Iqbal <mehtabi@clemson.edu>
2026-01-13 20:01:27 -0500

    updated layouts

--- src/layouts/templates/ContentBlock.tsx
+++ src/layouts/templates/ContentBlock.tsx
@@ -1,57 +1,62 @@
-import { useQuery } from '@tanstack/react-query';
-import { useCallback, useEffect, useState } from 'react';
-import { useStudy } from 'rssa-api';
-import type { PageContent, SurveyItemResponse } from '../../types/rssa.types';
-import ItemBlock from './ItemBlock';
+import { useQuery } from "@tanstack/react-query";
+import { useCallback, useEffect, useState } from "react";
+import { useStudy } from "rssa-api";
+import type { PageContent, SurveyItemResponse } from "../../types/rssa.types";
+import ItemBlock from "./ItemBlock";
 
 interface ContentBlockProps {
-    content: PageContent;
-    onComplete: (contentId: string) => void;
+  content: PageContent;
+  onComplete: (contentId: string) => void;
 }
 
 const ContentBlock: React.FC<ContentBlockProps> = ({ content, onComplete }) => {
-    const [answeredItemIds, setAnsweredItemIds] = useState<Set<string>>(new Set());
-    const { studyApi } = useStudy();
+  const [answeredItemIds, setAnsweredItemIds] = useState<Set<string>>(
+    new Set(),
+  );
+  const { studyApi } = useStudy();
 
-    const {
-        data: currentPageResponses,
-    } = useQuery({
-        queryKey: ['currentPageResponses', content.study_step_page_id],
-        queryFn: async () => studyApi.get<SurveyItemResponse[]>(`responses/survey/${content.study_step_page_id!}`),
-        select: (data) => (Array.isArray(data) ? data : []),
-        // If content or content.study_step_page_id is undefined, don't run the query.
-        enabled: !!content.study_step_page_id,
-        refetchOnWindowFocus: false,
-    });
+  const { data: currentPageResponses } = useQuery({
+    queryKey: ["currentPageResponses", content.study_step_page_id],
+    queryFn: async () =>
+      studyApi.get<SurveyItemResponse[]>(
+        `responses/survey/${content.study_step_page_id!}`,
+      ),
+    select: (data) => (Array.isArray(data) ? data : []),
+    // If content or content.study_step_page_id is undefined, don't run the query.
+    enabled: !!content.study_step_page_id,
+    refetchOnWindowFocus: false,
+  });
 
-    const handleSelection = useCallback((itemId: string) => {
-        setAnsweredItemIds((prev) => new Set(prev).add(itemId));
-    }, []);
+  const handleSelection = useCallback((itemId: string) => {
+    setAnsweredItemIds((prev) => new Set(prev).add(itemId));
+  }, []);
 
-    useEffect(() => {
-        if (answeredItemIds.size === content.items.length) onComplete(content.id);
-    }, [answeredItemIds.size, content, onComplete]);
+  useEffect(() => {
+    if (answeredItemIds.size === content.items.length) {
+      onComplete(content.id);
+    }
+  }, [answeredItemIds.size, content, onComplete]);
 
-    return (
-        <div className="justify-items-center">
-            {content.items.map((item) => {
-                const currentResponses = currentPageResponses?.find(
-                    (itemRes: SurveyItemResponse) => itemRes.survey_item_id === item.id
-                );
-                return (
-                    <ItemBlock
-                        contextTag={content.display_name}
-                        pageId={content.study_step_page_id}
-                        key={item.id}
-                        item={item}
-                        initialResponse={currentResponses}
-                        scaleLevels={content.scale_levels}
-                        onSelected={handleSelection}
-                    />
-                );
-            })}
-        </div>
-    );
+  return (
+    <div className="justify-items-center">
+      {content.items.map((item) => {
+        const currentResponses = currentPageResponses?.find(
+          (itemRes: SurveyItemResponse) => itemRes.survey_item_id === item.id,
+        );
+        return (
+          <ItemBlock
+            contextTag={content.display_name}
+            pageId={content.study_step_page_id}
+            key={item.id}
+            item={item}
+            initialResponse={currentResponses}
+            scaleLevels={content.scale_levels}
+            onSelected={handleSelection}
+          />
+        );
+      })}
+    </div>
+  );
 };
 
 export default ContentBlock;
--- src/layouts/templates/SurveyTemplate.tsx
+++ src/layouts/templates/SurveyTemplate.tsx
@@ -1,39 +1,42 @@
-import { useCallback, useEffect, useMemo, useState } from 'react';
-import { usePageCompletion } from '../../hooks/usePageCompletion';
-import type { SurveyPageType } from '../../types/rssa.types';
-import ContentBlock from './ContentBlock';
+import { useCallback, useEffect, useMemo, useState } from "react";
+import { usePageCompletion } from "../../hooks/usePageCompletion";
+import type { SurveyPageType } from "../../types/rssa.types";
+import ContentBlock from "./ContentBlock";
 
 interface SurveyTemplateProps {
-    surveyPage: SurveyPageType;
+  surveyPage: SurveyPageType;
 }
 
 const SurveyTemplate: React.FC<SurveyTemplateProps> = ({ surveyPage }) => {
-    const [completedContentIds, setCompletedContentIds] = useState<Set<string>>(new Set());
-    const { setIsPageComplete } = usePageCompletion();
+  const [completedContentIds, setCompletedContentIds] = useState<Set<string>>(
+    new Set(),
+  );
+  const { setIsPageComplete } = usePageCompletion();
 
-    const handelCompletion = useCallback((contentId: string) => {
-        setCompletedContentIds((prev) => new Set(prev).add(contentId));
-    }, []);
+  const handelCompletion = useCallback((contentId: string) => {
+    setCompletedContentIds((prev) => new Set(prev).add(contentId));
+  }, []);
 
-    const pageContents = useMemo(() => {
-        if (!surveyPage.study_step_page_contents) return [];
-        return surveyPage.study_step_page_contents;
-    }, [surveyPage]);
+  const pageContents = useMemo(() => {
+    if (!surveyPage.study_step_page_contents) return [];
+    return surveyPage.study_step_page_contents;
+  }, [surveyPage]);
 
-    useEffect(() => {
-        if (completedContentIds.size === pageContents.length) setIsPageComplete(true);
-    });
+  useEffect(() => {
+    if (completedContentIds.size === pageContents.length)
+      setIsPageComplete(true);
+  }, [completedContentIds, pageContents, setIsPageComplete]);
 
-    return (
-        <>
-            {pageContents.map((pageContent, index) => (
-                <div key={pageContent.id + '_' + index}>
-                    <p>{pageContent.preamble || ''}</p>
-                    <ContentBlock content={pageContent} onComplete={handelCompletion} />
-                </div>
-            ))}
-        </>
-    );
+  return (
+    <>
+      {pageContents.map((pageContent, index) => (
+        <div key={pageContent.id + "_" + index}>
+          <p>{pageContent.preamble || ""}</p>
+          <ContentBlock content={pageContent} onComplete={handelCompletion} />
+        </div>
+      ))}
+    </>
+  );
 };
 
 export default SurveyTemplate;
